50fd7bfa4c11f208e6ebac8281d3f6312c0a8dba
Ignacio Bonilla <ignacio.bonilla@live.u-tad.com>
2024-06-01 06:01:41 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListComponent } from './pages/list/list.component';
+import { FormComponent } from './pages/form/form.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListComponent,
+    FormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
